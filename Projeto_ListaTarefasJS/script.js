2e31b470218ec7651c43c342b037002465c9f9e3
BrunoSantosLira <brunomasterchif@gmail.com>
2023-01-16 22:35:47 -0300

    atualizacao_conteudo

--- Projeto_ListaTarefasJS/script.js
+++ Projeto_ListaTarefasJS/script.js
@@ -1,6 +1,6 @@
-
 let tarefa = document.getElementById('tarefa')
 let caixa_tarefa = document.getElementById('caixa-tarefas')
+
 
 
 function adicionar(){
@@ -12,6 +12,7 @@ function adicionar(){
         //Editando elemento div
         linha_tarefa.className = 'border border-success mt-2'
         linha_tarefa.style.padding = '15px'
+        
 
         //Adicionando elemento na div criada
         linha_tarefa.innerHTML = tarefa.value
@@ -19,9 +20,19 @@ function adicionar(){
 
         localStorage.setItem(gerarID(),JSON.stringify(linha_tarefa.innerHTML))
 
+        let btn = document.createElement('button')
+        btn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+        btn.className = 'btn btn-danger'
+        btn.style.width = '80px'
+        btn.style.height = '40px'
+        btn.style.float = 'right'
+
+        linha_tarefa.append(btn)
+
         tarefa.value = ''
+   
+        window.location.reload()
     }
-    
     
 }
 
@@ -37,4 +48,61 @@ function gerarID(){
     localStorage.id = parseInt(localStorage.id) + 1
     return parseInt(proximoID) + 1
 
+}
+
+
+
+
+function mostrar_tarefas(){
+    let qtd_elementos = localStorage.getItem('id')
+
+    if(localStorage.length > 1){
+
+        for(let c = 1; c <= qtd_elementos;c++){
+            let elemento = JSON.parse(localStorage.getItem(c))
+            
+
+            if(elemento ===null){
+                continue
+            }
+
+
+
+            let linha_tarefa = document.createElement('div')
+
+            //Editando elemento div
+            linha_tarefa.className = 'border border-success mt-2'
+            linha_tarefa.id = c
+            linha_tarefa.style.padding = '15px'
+
+            //Adicionando elemento na div criada
+            linha_tarefa.innerHTML = elemento
+            caixa_tarefa.append(linha_tarefa)    
+
+            
+
+            let btn = document.createElement('button')
+            btn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+            btn.className = 'btn btn-danger'
+            btn.id = c
+            btn.style.width = '80px'
+            btn.style.height = '40px'
+            btn.style.float = 'right'
+            btn.onclick = function(){
+                localStorage.removeItem(c)
+               
+                linha_tarefa.style.display = 'none'
+            }
+
+            linha_tarefa.append(btn)
+            console.log(linha_tarefa.id)
+
+        }
+
+
+    }
+}
+
+function numeroaleatorio(){
+    return Math.trunc(Math.random() * 1000 +1)
 }
